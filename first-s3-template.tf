a90d411106fde953ca78540c864bc78281d2a60f
manojkumaRre <manojcool251@gmail.com>
2020-09-19 15:41:18 +0530

    push new code

--- first-s3-template.tf
+++ first-s3-template.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   version = "~> 2.0"
   region  = "ap-southeast-2"
-  AccessKey = var.AccessKey
-  SecretKey = var.SecretKey
+  access_key = var.AccessKey
+  secret_key = var.SecretKey
 }
 resource "aws_s3_bucket" "b" {
   bucket = "test1234"
